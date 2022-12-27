d1ff9cb8c760bd7bc26682db581fa62d37323f6e
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2022-12-27 17:36:57 +0100

    fixes

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log(12 % 1)
+// Selectors
+const buttons = Array.from(document.querySelectorAll('.button'))
+const display = document.querySelector('.display')
+buttons.map(btn => {
+    btn.addEventListener('click', (e) => {
+        if (e.target.innerText === '=') {
+            try {
+                return display.innerHTML = eval(display.innerText)
+            } catch (error) {
+                display.innerHTML = 'Error Input'
+
+            }
+
+        } else if (e.target.textContent === 'AC') {
+            return display.textContent = ''
+        } else if (e.target.textContent === '␡') {
+            return display.textContent = display.innerHTML.slice(0, -1)
+        } else if (e.target.innerHTML === '×') {
+            display.textContent += '*'
+        } else if (e.target.innerHTML === '÷') {
+            display.textContent += '/'
+        } else {
+            display.textContent += e.target.innerText
+
+        }
+
+    })
+})
